72582df597d4709c277964ace455b8a120d9fbb8
Joshua Bowman <jbowmanoize@gmail.com>
2021-04-16 10:14:10 -0700

    Add files via upload

---
+++ Project_Flashcards_Qualified_1/README.md
@@ -0,0 +1,3 @@
+# FlashCard
+# FlashCard
+# FlashCardsApp
---
+++ Project_Flashcards_Qualified_1/src/Layout/index.js
@@ -0,0 +1,51 @@
+import React, { Fragment, useState } from "react";
+import { Route } from "react-router";
+import Header from "./Header";
+
+import { Switch } from "react-router-dom";
+import DeckList from "./decks/DeckList";
+import DecksNav from "./decks/DecksNav";
+import NotFound from "./NotFound";
+
+function Layout() {
+  const [decks, setDecks] = useState([]);
+  return (
+    <Fragment>
+      <Header />
+      <div className="container px-5">
+        {/* TODO: Implement the screen starting here */}
+        <Switch>
+          <Route path="/decks">
+            <DecksNav setDecks={setDecks} decks={decks} />
+          </Route>
+          <Route exact path="/">
+            <DeckList setDecks={setDecks} decks={decks} />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Layout;
+
+// import React from "react";
+// import Header from "./Header";
+// import NotFound from "./NotFound";
+
+// function Layout() {
+//   return (
+//     <>
+//       <Header />
+//       <div className="container">
+//         {/* TODO: Implement the screen starting here */}
+//         <NotFound />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Layout;
